262187393fc20ba1f00df4d62e6b4b58ca2daf8d
Tom Milligan <tmilligan@skytap.com>
2015-07-27 17:32:55 -0500

    Add License Text

--- Source/SkytapREST/Utilities/Credentials.cs
+++ Source/SkytapREST/Utilities/Credentials.cs
@@ -1,9 +1,22 @@
 ﻿// 
 // Credentials.cs
-// 
-// Copyright (c) Skytap, Inc. All rights reserved.
-// 
-namespace Skytap.Cloud.Utilities
+/**
+ * Copyright 2014 Skytap Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+ 
+ namespace Skytap.Cloud.Utilities
 {
     /// <summary>
     /// Wrapper around a user's name and password/key combination.
